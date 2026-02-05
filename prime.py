4165b7dd23f1bc2f9dcb85c4a34888c6851da43f
Pranavi Balakrishnan <pranu9593@gmail.com>
2026-02-05 14:25:35 +0530

    Add prime number checker in prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+# prime.py
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            print(f"{num} is not prime")
+            break
+    else:
+        print(f"{num} is prime")
+else:
+    print(f"{num} is not prime")
